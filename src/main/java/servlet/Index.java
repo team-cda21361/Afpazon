6d05c4a306d43b378a24ba5678f8bd41aade9ee3
Charles Fouquet <fouquetcharles@gmail.com>
2023-04-11 16:56:01 +0200

    Update Index.java

--- src/main/java/servlet/Index.java
+++ src/main/java/servlet/Index.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import beans.Role;
 import beans.User;
 import dao.CategoryDao;
 import dao.ProductDao;
