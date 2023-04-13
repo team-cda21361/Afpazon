948e55019a1b10215fe4cfdcb19e63ea8a884d4d
Jose AQUINO <angelexe8@hotmail.com>
2023-04-13 04:44:18 +0200

    Panier 2er partie

--- src/main/java/beans/Cart.java
+++ src/main/java/beans/Cart.java
@@ -3,6 +3,7 @@ package beans;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 
+
 public class Cart{
     
     private ArrayList<Item> Items = new ArrayList<>();
@@ -47,5 +48,53 @@ public class Cart{
 		}
 		return df.format(count);
 	}
+    
+    
+	
+	//ADD QTE 
+	public void addQte(int idItem) {
+		for(Item  item: Items) {
+			
+			if(item.getProduct().getId()==idItem) {
+				item.setQuantity(item.getQuantity()+1);
+				System.out.println("Quantite Add OK.");
+				break;
+			}
+		}
+	}
+	//REMOVE QTE 
+	public void removeQte(int idItem) {
+		for(Item  item: Items) {
+			
+			if(item.getProduct().getId()==idItem) {
+				if(item.getQuantity() <= 1) {
+					item.setQuantity(1);
+					System.out.println("Quantite Add OK NO POSIBLE. -1: ");
+				}else {
+					item.setQuantity(item.getQuantity()-1);
+					System.out.println("Quantite Add OK.");
+				}
+
+				break;
+			}
+		}
+	}
+	
+	//DELETEITEM
+		public boolean deleteById(int idItem) {
+			for(Item  item: Items) {
+				
+				if(item.getProduct().getId()==idItem) {
+					Items.remove(item);
+					System.out.println("Delete Article de panier OK.");
+					return true;
+				}
+			}
+			return false;
+		}
+	
+	
+	
+	
     
 }
