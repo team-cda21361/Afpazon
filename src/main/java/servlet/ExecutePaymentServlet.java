8cfe2e7edc40b98ed54d327edaffb7bea2abe328
Marc <segardmarc@gmail.com>
2023-04-13 16:54:37 +0200

    marc paypal mdp payment: Test1234&

---
+++ src/main/java/servlet/ExecutePaymentServlet.java
@@ -0,0 +1,62 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.paypal.api.payments.*;
+import com.paypal.base.rest.PayPalRESTException;
+
+import paypal.PaymentServices;
+/**
+ * Servlet implementation class ExecutePaymentServlet
+ */
+@WebServlet("/execute_payment")
+public class ExecutePaymentServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ExecutePaymentServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String paymentId = request.getParameter("paymentId");
+        String payerId = request.getParameter("PayerID");
+ 
+        try {
+            PaymentServices paymentServices = new PaymentServices();
+            com.paypal.api.payments.Payment payment = paymentServices.executePayment(paymentId, payerId);
+             
+            PayerInfo payerInfo = payment.getPayer().getPayerInfo();
+            Transaction transaction = payment.getTransactions().get(0);
+             
+            request.setAttribute("payer", payerInfo);
+            request.setAttribute("transaction", transaction);          
+ 
+            request.getRequestDispatcher("/view/paypal_receipt.jsp").forward(request, response);
+             
+        } catch (PayPalRESTException ex) {
+            request.setAttribute("errorMessage", ex.getMessage());
+            ex.printStackTrace();
+            request.getRequestDispatcher("/view/paypal_error.jsp").forward(request, response);
+        }
+    }
+}
---
+++ src/main/webapp/view/paypal_error.jsp
@@ -0,0 +1,6 @@
+<div align="center">
+    <h1>Payment Error</h1>
+    <br/>
+    <h3>${errorMessage}</h3>
+    <br/>
+</div>
